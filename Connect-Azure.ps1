257353e23bba04d6eb0ee84fc3d8c31ad9d001bd
kennethgreig36 <58401507+kennethgreig36@users.noreply.github.com>
2019-12-26 04:55:05 +0000

    Added 2 new files

---
+++ Connect-Azure.ps1
@@ -0,0 +1,6 @@
+$password = ConvertTo-SecureString 'Kut43749' -AsPlainText -Force
+$credential = New-Object System.Management.Automation.PSCredential ('IT.Support@symatrix.com', $password)
+
+Connect-AzureRmAccount -Credential $credential -Subscription 'e9b5c6f5-684c-4f14-8d62-607320da1d8c' -Tenant '885c456e-183b-4491-aa31-37685ebf3d2e'
+
+Get-AzureRmTenant
