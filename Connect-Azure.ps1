aad3adf5792428eda000047b953157e8fb082d18
kennethgreig36 <58401507+kennethgreig36@users.noreply.github.com>
2019-12-26 06:05:16 +0000

    Updated powershell scripts

--- Connect-Azure.ps1
+++ Connect-Azure.ps1
@@ -3,4 +3,4 @@ $credential = New-Object System.Management.Automation.PSCredential ('IT.Support@
 
 Connect-AzureRmAccount -Credential $credential -Subscription 'e9b5c6f5-684c-4f14-8d62-607320da1d8c' -Tenant '885c456e-183b-4491-aa31-37685ebf3d2e'
 
-Get-AzureRmTenant
+Remove-AzContainerGroup -ResourceGroupName "Kens_Containers" -Name "supertscontainer"
--- PowerShell_Azure_DeployContainer.ps1
+++ PowerShell_Azure_DeployContainer.ps1
@@ -1,4 +1,7 @@
-Login-AzureRmAccount
+$password = ConvertTo-SecureString 'Kut43749' -AsPlainText -Force
+$credential = New-Object System.Management.Automation.PSCredential ('IT.Support@symatrix.com', $password)
+
+Connect-AzureRmAccount -Credential $credential -Subscription 'e9b5c6f5-684c-4f14-8d62-607320da1d8c' -Tenant '885c456e-183b-4491-aa31-37685ebf3d2e'
 
 $resourceGroup = Get-AzureRmResourceGroup -Name 'Kens_Containers'
 
@@ -10,3 +13,7 @@ $ContainerParams = @{
     DnsNameLabel = 'supertscontainer'
 }
 New-AzureRmContainerGroup @ContainerParams
+
+#Get-AzContainerGroup -ResourceGroupName "Kens_Containers" -Name "supertscontainer"
+
+#Remove-AzureRmContainerGroup -ResourceGroupName "Kens_Containers" -Name "supertscontainer"
