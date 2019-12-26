3355535167a40b461593a2e710ab36ddbac6694d
kennethgreig36 <58401507+kennethgreig36@users.noreply.github.com>
2019-12-26 17:59:03 +0000

    Added docker files

---
+++ Create_Key_Vault.ps1
@@ -0,0 +1,7 @@
+#$ACR_NAME= "ContainerRegistrykge865"
+
+#$RES_GROUP= $ACR_NAME # Resource Group name
+
+#az group create --resource-group $RES_GROUP --location westeurope
+#az acr create --resource-group $RES_GROUP --name $ACR_NAME --sku Standard --location westeurope
+
